4704f9f579dd858b62ccccd70633f4010261165d
Anton Kachurin <katchuring@gmail.com>
2025-05-02 23:24:45 +0300

    Initial implementation (#1)
    Provided functionality
      - Start KoboldCpp with list of given args
      - Stop managed KoboldCpp run
      - Check currently run model
    
    Added API documentation for plugin

---
+++ src/endpoints.ts
@@ -0,0 +1,75 @@
+import chalk from 'chalk';
+import { RequestHandler } from 'express';
+import { MODULE_NAME } from './consts';
+import { Controller, getLoadedModelName } from './kobold';
+
+interface getRunningModelResponse {
+    model: string | undefined
+    error: string | undefined
+}
+
+export class Handlers {
+    controller: Controller
+
+    constructor(controller: Controller) {
+        this.controller = controller
+    }
+
+    getRunningModel: RequestHandler = async (_, res) => {
+        let status = 200
+        const data: getRunningModelResponse = { model: undefined, error: undefined }
+
+        await getLoadedModelName()
+            .then(name => data.model = name)
+            .catch(err => {
+                data.error = 'error requesting koboldcpp running model'
+                status = 500
+                console.error(chalk.redBright(MODULE_NAME), 'KoboldCpp unavailable')
+                console.error(err)
+            })
+
+        return res.status(status).json(data)
+    }
+
+    postModel: RequestHandler = async (req, res) => {
+        await this
+            .controller
+            .runKoboldCpp(req.body)
+            .then(() => {
+                return res.status(201).send()
+            })
+            .catch(
+                (err) => {
+                    console.error(chalk.redBright(MODULE_NAME), 'KoboldCpp unavailable')
+                    console.error(err)
+
+                    return res.status(500).json({ error: 'error starting koboldcpp' })
+                },
+            )
+    }
+
+    deleteModel: RequestHandler = async (_, res) => {
+        await this
+            .controller
+            .stopKoboldCpp()
+            .catch(/* well, no luck */)
+
+        return res.status(204).send()
+    }
+
+    openApiYaml: RequestHandler = (_, res) => {
+        return res.sendFile('openapi.yaml', { root: `${__dirname}/..` })
+    }
+
+    redoc: RequestHandler = (req, res) => {
+        const htmlBody = '<html><body>' +
+            `<redoc spec-url="${req.baseUrl}/openapi.yaml"></redoc>` +
+            '<script src="https://cdn.redoc.ly/redoc/latest/bundles/redoc.standalone.js"> </script>' +
+            '</html>'
+
+        return res
+            .status(200)
+            .setHeader('content-type', 'text/html')
+            .send(htmlBody)
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import bodyParser from 'body-parser';
 import { Router } from 'express';
-import { Chalk } from 'chalk';
+import { chalk, MODULE_NAME } from './consts';
+import { Handlers } from './endpoints';
+import { Controller } from './kobold';
+
 
 interface PluginInfo {
     id: string;
@@ -8,53 +11,44 @@ interface PluginInfo {
     description: string;
 }
 
-interface Plugin {
-    init: (router: Router) => Promise<void>;
-    exit: () => Promise<void>;
-    info: PluginInfo;
+class KoboldRunnerPlugin {
+    info: PluginInfo = {
+        id: 'kobold-switcher',
+        name: 'Koboldcpp Model switch Plugin',
+        description: 'A plugin to reload locally running koboldcpp with different flags.',
+    };
+
+    jsonParser = bodyParser.json()
+    controller = new Controller()
+    handlers = new Handlers(this.controller)
+
+    /**
+    * Initialize the plugin.
+    * @param router Express Router
+    */
+    init = async (router: Router) => {
+        // Used to check if the server plugin is running
+        router.get('/probe', (_, res) => {
+            return res.sendStatus(204);
+        });
+        // doc
+        router.get('/redoc', this.handlers.redoc)
+        router.get('/openapi.yaml', this.handlers.openApiYaml)
+        // models
+        router.get('/model', this.handlers.getRunningModel);
+        router.post('/model', this.jsonParser, this.handlers.postModel);
+        router.delete('/model', this.jsonParser, this.handlers.deleteModel);
+
+        console.log(chalk.green(MODULE_NAME), 'Plugin loaded!');
+    }
+
+    exit = async () => {
+        await this.controller.stopKoboldCpp()
+
+        console.log(chalk.yellow(MODULE_NAME), 'Plugin exited');
+    }
 }
 
-const chalk = new Chalk();
-const MODULE_NAME = '[SillyTavern-Example-Plugin]';
-
-/**
- * Initialize the plugin.
- * @param router Express Router
- */
-export async function init(router: Router): Promise<void> {
-    const jsonParser = bodyParser.json();
-    // Used to check if the server plugin is running
-    router.post('/probe', (_req, res) => {
-        return res.sendStatus(204);
-    });
-    // Use body-parser to parse the request body
-    router.post('/ping', jsonParser, async (req, res) => {
-        try {
-            const { message } = req.body;
-            return res.json({ message: `Pong! ${message}` });
-        } catch (error) {
-            console.error(chalk.red(MODULE_NAME), 'Request failed', error);
-            return res.status(500).send('Internal Server Error');
-        }
-    });
-
-    console.log(chalk.green(MODULE_NAME), 'Plugin loaded!');
-}
-
-export async function exit(): Promise<void> {
-    console.log(chalk.yellow(MODULE_NAME), 'Plugin exited');
-}
-
-export const info: PluginInfo = {
-    id: 'example',
-    name: 'Example Plugin',
-    description: 'A simple example plugin for SillyTavern server.',
-};
-
-const plugin: Plugin = {
-    init,
-    exit,
-    info,
-};
+const plugin = new KoboldRunnerPlugin()
 
 export default plugin;
