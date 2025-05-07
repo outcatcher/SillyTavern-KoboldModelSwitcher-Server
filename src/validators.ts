a4a54509f98e505818c115682cefef3c0e49bb27
Anton Kachurin <katchuring@gmail.com>
2025-05-07 23:02:25 +0300

    Fix create model request validation

--- src/validators.ts
+++ src/validators.ts
@@ -1,20 +1,22 @@
 import { allowedContextSizes } from './consts';
 
 //  {
-//     "contextSize": 12288,
-//     "gpuLayers": 81,
-//     "model": "nvidia_Llama-3_3-Nemotron-Super-49B-v1-Q4_K_S.gguf",
-//     "threads": 1,
-//     "tensorSplit": [29, 52],
-//   }
+//    "contextSize": 12288,
+//    "gpuLayers": 81,
+//    "model": "nvidia_Llama-3_3-Nemotron-Super-49B-v1-Q4_K_S.gguf",
+//    "threads": 1,
+//    "tensorSplit": [29, 52],
+//  }
 export const modelSchema = {
     contextSize: {
+        optional: true,
         isIn: {
             errorMessage: `contextSize must be one of [${allowedContextSizes.join(', ')}]`,
             options: [allowedContextSizes],
         },
     },
     gpuLayers: {
+        optional: true,
         isInt: {
             errorMessage: 'gpuLayers must be positive integer',
             options: {
@@ -27,15 +29,25 @@ export const modelSchema = {
             errorMessage: 'model is required',
         },
     },
+    tensorSplit: {
+        optional: true,
+        isArray: {
+            errorMessage: 'tensorSplit must be float array with minimal length of two',
+            options: {
+                min: 2,
+            },
+        },
+    },
     'tensorSplit.*': {
         isFloat: {
-            errorMessage: 'tensor split values must be valid floats',
+            errorMessage: 'tensorSplit values must be valid floats',
             options: {
                 min: 0,
             },
         },
     },
     threads: {
+        optional: true,
         isInt: {
             errorMessage: 'threads must be positive integer',
             options: {
