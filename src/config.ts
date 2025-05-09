8a0267a9533d6590929a6768512b92e8b29213d5
Anton Kachurin <katchuring@gmail.com>
2025-05-09 22:29:53 +0300

    Implement cofiguration handling (#12)
    Add configuration loading
    
    Fix error handling middleware

---
+++ src/config.ts
@@ -0,0 +1,51 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
+import { chalk, MODULE_NAME } from './consts';
+import { ConfigurationError } from './errors';
+
+const pluginGitName = 'SillyTavern-KoboldModelSwitcher-Server'
+
+interface Config {
+    basePath: string
+}
+
+const defaultConfig: Config = {
+    basePath: '',
+}
+
+interface FSError {
+    code: string
+    path: string
+}
+
+const configPath = path.join(process.cwd(), `./plugins/${pluginGitName}/config.json`)
+
+const readConfig = (): Config => {
+    try {
+        const configFile = fs.readFileSync(configPath, 'utf8')
+
+        return JSON.parse(configFile.toString()) as Config
+    } catch (err: unknown) {
+        if ((err as FSError).code === 'ENOENT') {
+            fs.writeFileSync(configPath, JSON.stringify(defaultConfig, undefined, "  "))
+
+            globalThis.console.log(chalk.red(MODULE_NAME),
+                `Configuration missing. Configuration template created at ${configPath}`);
+
+            return readConfig()
+        }
+
+        throw err
+    }
+}
+
+const validateConfig = (cfg: Config): Config => {
+    if (!path.isAbsolute(cfg.basePath)) {
+        throw new ConfigurationError('basePath must be absolute')
+    }
+
+    return cfg
+}
+
+export const config = validateConfig(readConfig())
--- src/errors.ts
+++ src/errors.ts
@@ -1,6 +1,7 @@
 /*eslint max-classes-per-file: "off"*/
 
 export class ModelStateError extends Error { }
+
 export class RequestValidationError extends Error {
     messages: string[]
 
@@ -8,5 +9,11 @@ export class RequestValidationError extends Error {
         super(messages.join('\n'))
 
         this.messages = messages
+    }
+}
+
+export class ConfigurationError extends Error {
+    constructor(msg: string) {
+        super(`Configuration error: ${msg}`)
     }
 }
